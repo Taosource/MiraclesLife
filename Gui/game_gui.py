90d3c80bd2ead88719efd76895fc3b274a800625
CaramelCof <109976394+CaramelCof@users.noreply.github.com>
2022-10-06 12:52:15 +0800

    画gui

---
+++ Gui/game_gui.py
@@ -0,0 +1,37 @@
+
+import pygame
+from setting import Value_base
+
+class Gui:
+    def __init__(self):
+        self.settings = Value_base()
+        self.game_name = self.settings.game_name
+        self.image = self.settings.image
+        self.ship = self.settings.ship
+        self.gui = self.game_gui
+
+    def game_gui(self):
+        """游戏界面gui"""
+        pygame.init()
+        pygame.display.set_caption(self.game_name)
+        run = True
+        while run:
+            for event in pygame.event.get():
+                screen_size = event.size
+                guis = pygame.display.set_mode(screen_size)
+                gui_bmp = self.gui
+                guis.blit(gui_bmp, (0, 0))
+                pygame.display.update()
+                if event.type == pygame.QUIT:  # 判断用户是否点了"X"关闭按钮,并执行if代码段
+                    pygame.quit()
+                    run = False
+
+
+
+
+
+
+
+
+
+
